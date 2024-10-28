c2e3967366cd07c6215b09701e7bc147ba81295c
Manankumar6 <manankumar2019@gmail.com>
2024-10-28 10:44:49 +0530

    chagne next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Apply these headers to all API routes
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://nextjs-first-phpx.vercel.app', // Adjust to your frontend URL or use '*' for all
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, PATCH, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type, Authorization',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
