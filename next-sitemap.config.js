01992d68a72a624d36335f33dbeb7df4a7518e2c
Manankumar6 <manankumar2019@gmail.com>
2024-11-06 20:15:43 +0530

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://techcanva.in', // Replace with your domain
+    generateRobotsTxt: true,           // Generates a robots.txt file
+  };
+  
